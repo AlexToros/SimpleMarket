7772be494203aac815fda356c77b7f87ff3e5b27
Alex Pyankov <pyankov.work@gmail.com>
2018-07-19 15:59:05 +0300

    Add Pages to Listing

--- Pages/Listing.aspx.cs
+++ Pages/Listing.aspx.cs
@@ -10,9 +10,23 @@ namespace GameStore
     public partial class Listing : System.Web.UI.Page
     {
         Repository rep = new Repository();
+
+        private int pageSize = 4;
+
+        protected int CurrentPage
+        {
+            get {
+                int page;
+                return int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+            }
+        }
+
         protected IEnumerable<Game> GetGames()
         {
-            return rep.Games;
+            return rep.Games
+                .OrderBy(g => g.GameID)
+                .Skip((CurrentPage - 1) * pageSize)
+                .Take(pageSize);
         }
         protected void Page_Load(object sender, EventArgs e)
         {
