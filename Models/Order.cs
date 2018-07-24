1bb5825f68f21e0698ca21caf5da1d47e1304998
Alex Pyankov <pyankov.work@gmail.com>
2018-07-24 13:58:08 +0300

    Add error messages

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore
 {
     public class Order
     {
         public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите свое имя")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Вы должны указать хотя бы один адрес")]
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Укажите город")]
         public string City { get; set; }
         public bool GiftWrap { get; set; }
         public bool Dispatched { get; set; }
